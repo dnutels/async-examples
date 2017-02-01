22d87c9377116e6b5fe0b494a75515de6cd2d083
dnutels <dmitry.nutels@naturalint.com>
2017-02-01 17:52:26 +0200

    Add loop examples

--- tests/loops.js
+++ tests/loops.js
@@ -5,26 +5,21 @@ const Logger = require('../lib/common/logger');
 const {request, requestPromise} = require('./common');
 
 function getFriends() {
-    request.get({url: '/users/?filter=[uid1,uid2,uid3]'}, (err, response) => {
+    request.get({url: '/users/?filter=[cid1,cid2,cid8]'}, (err, {body: users}) => {
         if (!err) {
-            const {body: users} = response;
-            const friends = new Set();
+            const friends = [];
+
             let count = users.length;
 
-            users.forEach((user) => {
-                const {id} = user;
-                request.get({url: `/friends/${id}`}, (err, response) => {
+            users.forEach(({id}) => {
+                request.get({url: `/friends/${id}`}, (err, {body: userFriends}) => {
                     if (!err) {
+                        friends.push(...userFriends);
+
                         count -= 1;
 
                         if (count === 0) {
-                            Logger.info(friends);
-                        } else {
-                            const {body: userFriends} = response;
-
-                            userFriends.forEach((friend) => {
-                                friends.add(friend);
-                            });
+                            Logger.info(new Set(friends));
                         }
                     } else {
                         Logger.error(err);
@@ -38,9 +33,74 @@ function getFriends() {
     });
 }
 
-function getFriendsPromise() {
+function processFriends(friendsForAllUsers) {
+    const friends = friendsForAllUsers.reduce((aggregator, userFriends) => {
+        aggregator.push(...userFriends);
+        return aggregator;
+    }, []);
 
+    return friends;
+}
+
+function getFriendsPromise() {
+    requestPromise.get({url: '/users/?filter=[cid1,cid2,cid8]'})
+        .then((users) => {
+            const friendRequests = users.map(({id}) => {
+                return requestPromise.get({url: `/friends/${id}`});
+            });
+
+            return Promise.all(friendRequests);
+        })
+        .then((friendsForAllUsers) => {
+            const friends = processFriends(friendsForAllUsers);
+
+            Logger.info(new Set(friends));
+        })
+        .catch((err) => {
+            Logger.error(err);
+        });
+}
+
+async function getFriendsAsync1() {
+    try {
+        const users = await requestPromise.get({url: '/users/?filter=[cid1,cid2,cid8]'});
+
+        const friendsForAllUsers = users.map(async ({id}) => {
+            return await requestPromise.get({url: `/friends/${id}`});
+        });
+
+        const friends = processFriends(friendsForAllUsers);
+
+        Logger.info(new Set(friends));
+    } catch(err) {
+        Logger.error(err);
+    }
+}
+
+function getUsers() {
+    return requestPromise.get({url: '/users/?filter=[cid1,cid2,cid8]'});
+}
+
+function getFriend(id) {
+    return requestPromise.get({url: `/friends/${id}`});
+}
+
+async function getFriendsAsync2() {
+    try {
+        const users = await getUsers()
+
+        const friendRequests = users.map(({id}) => getFriend(id));
+        const friendsForAllUsers = await Promise.all(friendRequests);
+
+        const friends = processFriends(friendsForAllUsers);
+
+        Logger.info(new Set(friends));
+    } catch(err) {
+        Logger.error(err);
+    }
 }
 
 getFriends();
 getFriendsPromise();
+// getFriendsAsync1();
+getFriendsAsync2();
